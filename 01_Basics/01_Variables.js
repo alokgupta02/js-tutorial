10ad1c06900d94f882daf6fff2ea887add7a8e23
Alok Gupta <114176751+alokgupta02@users.noreply.github.com>
2024-04-11 12:00:39 +0000

    saving

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 12312
+let accountEmail = "alok@boom.com"
+var accountPassword = "12345"
+city = "Bhopal"
+//accountId = 12 // not allowed
+
+accountEmail = "aa@aa.com"
+accountPassword = "2wxd"
+city = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, city])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+// datatype conversion
+
+let score = "33"
+
+//console.log(typeof score)
+let num = Number(score)
+//console.log(typeof num)
+
+const heros = ["shaktimaan","hulk","vyom"]
+console.log(heros)
+
+let myObj = {
+    name: "Alok Gupta",
+    city: "Bhopal",
+    age: 34
+}
+
+console.log(myObj);
+
+const greetPeople = function(){
+    console.log("Hello!!");
+}
